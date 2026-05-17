e6a3050111ea603ef1aadf599a5eff0e91d51a65
neural-anand <ao7047661@gmail.com>
2026-05-17 14:02:56 +0530

    Create temperature_converter.py

---
+++ temperature_converter.py
@@ -0,0 +1,16 @@
+
+# temperature converter c to f
+
+
+c = float(input("enter celsius="))
+
+f = (c *9/5) + 32 
+print("Fahrenheit = ", f)
+
+# temp converter f to c
+
+
+
+f = float(input("enter fahrenheit = "))
+c = (f - 32) * 5/9
+print("celsius = ", c)
