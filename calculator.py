0db41fee14daa1b43ac66142a18222a655180229
neural-anand <ao7047661@gmail.com>
2026-05-17 14:01:59 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#   CALCULATOR
+
+a=int(input("Enter first enter = "))
+b = int(input("Enter second number = "))
+
+
+
+
+print("Addition =",a+b)
+print("multiplication =",a*b)
+print("difference = ",a-b)
+print("division =" , a/b)
